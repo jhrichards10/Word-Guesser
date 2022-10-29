e60ce2c3ead363fc98186b7f8c3499047afbdc2b
jhrichards10 <114703580+jhrichards10@users.noreply.github.com>
2022-10-29 18:58:24 +0000

    Fixed the formatting and added counting strikes

--- main.py
+++ main.py
@@ -1,29 +1,61 @@
-# Create a functoin to set up print guess accuracy
-def printGuessAccuracy(guess, actual):
-  for index in cycle():
-    letter = guess[index]
-    secretWord = actual
-    if letter in secretWord:
-      if(letter == secetWord[index]):
-        
+# Create secret word
+secretWord = "hotwheels"
 
-
-
-
-
+# Create a list for the user guesses
+userGuesses = []
 
 # Create function for user guess input
 def getUserGuess():
+  
   # Open userGuess to take in a letter
   userGuess = ""
+  
   # Create while loop to take in the input
   while len(userGuess) != 1:
+    
     # Create input to take in user guess
     userGuess = input("Enter a letter: ")
+    print()
+    
     # Return the user guess lowercase
     return userGuess.lower()
 
     
+# Create a function to displey letter or underscore
+def displaySecretWord(secretWord, userGuess):
+  letter = userGuesses
+  
+  # Search through each letter of secret word 
+  for letter in userGuess:
+    
+    # Create if statement for letter in word 
+    if(letter in secretWord):
+      print(letter, end="")
+    
+      # Else, letter not in word display "_"
+    else:
+      print("_", end="")
+  print()
+  
+# Create function if the user has won or not 
+def userWinOrLose(userGuesses, secretWord):
+  
+  # Create true variable 
+  win = True 
+  
+  # Create for loop to search word
+  for letter in secretWord:
+    
+    # Create if statement to check letter in guesses
+    if letter not in userGuesses:
+      
+      # Create false variable
+      win = False
+      break
+      
+# return the variable
+  return win
+  
 # Create a game header
 print(r"""
 __    __       ___      .__   __.   _______ .___  ___.      ___      .__   __. 
@@ -46,30 +78,43 @@ print()
 
 # Print Directions
 print("The player must enter in only a one letter guess. If they do not they will be re-prompted to do so.")
-print("Each player will have five tries to guess the correct answer.")
+print("Each player will have five incorrect tries to guess the correct answer.")
 print("Have fun and good luck!")
-
-# Create secret word
-secretWord = "hotwheels"
-
-# Create variable to hold user guess 
-guess = ""
+# Space
+print()
 
 # Keep track of attempts
-countattempts = 0
+countAttempts = 0
 
-# Create a while to keep track of attempts and guesses
-while countAttempts <5 and guess != secretWord:
+# Create a while loop game to keep track of attempts and guesses
+while countAttempts < 5 and not userWinOrLose(userGuesses, secretWord):
 
-  guess = getUserGuess()
-  printGuessAccruacy(guess, secretWord)
-  # Finish counting attempts
-  countAttempts += 1
+  # Calls the players input
+  userGuess = getUserGuess()
+  if userGuess in secretWord:
+  
+    # Add the user guesses together 
+    userGuesses.append(userGuess)
+    print("Correct guess!")
+    print()
+  
+    # if user guess incorrect
+  else:
+    print("Incorrect guess!")
+    print()
+    
+    # Count the incorrect attempts
+    countAttempts += 1
+    
+    # print statement for counting strikes 
+    print(f"{countAttempts} strike(s), you only get 5")
+  
+    # Calls the players input and compares to secret word
+  displaySecretWord(userGuesses, secretWord)
   print()
-  print()
 
-# Inform player if they have won or lost 
-if(guess == secretWord):
-  print("You won!")
+# Let the player know if they have won or lost
+if userWinOrLose(userGuesses, secretWord):
+  print("Congrats! You Won!")
 else:
-  print("You Lost")
+  print("You Lose! You have ran out of guesses.")
